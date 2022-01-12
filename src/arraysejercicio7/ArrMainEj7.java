7fb270102776a9c3bbd5ac9e842bd067c9054b26
SrPollo25 <ortegachkn25@gmail.com>
2022-01-12 19:26:19 +0100

    Empiezo el programa

---
+++ src/arraysejercicio7/ArrMainEj7.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraysejercicio7;
+
+import numbers.EntradaNumeros;
+
+/**
+ *
+ * @author casa
+ */
+public class ArrMainEj7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(EntradaNumeros.numIntGrater("hola: ", 5));
+        
+    }
+    
+}
---
+++ src/arraysejercicio7/Empleado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraysejercicio7;
+
+/**
+ *
+ * @author casa
+ */
+public class Empleado {
+    
+}
